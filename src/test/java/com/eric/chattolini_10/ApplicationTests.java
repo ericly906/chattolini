661fe149bb29f072301bcee0e893736f98e51c84
Eric <eric.ly906@berkeley.edu>
2021-12-12 20:55:18 -0800

    chattolini 1.0

---
+++ src/test/java/com/eric/chattolini_10/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.chattolini_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
