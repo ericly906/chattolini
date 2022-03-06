70e9972e6a8d9cd5f338b44ad113e679e56c95c0
Eric Ly <eric.ly906@berkeley.edu>
2022-03-05 17:00:10 -0800

    a space

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -148,7 +148,7 @@ function onMessageReceived(payload) {
         var usernameElement = document.createElement('span');
         var currentTime = new Date().toLocaleTimeString();
         var usernameText = document.createElement("usernameText");
-        usernameText.innerHTML = "<input type='button' value=" + message.sender + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + currentTime + "onclick='togglePopUp()' style='background: none;border: none;padding: 0;text-decoration: underline;cursor: pointer;font-size: 18px;font-weight: 600;color: #cfcfcf;'/><div class='popup' id='pop' style='display: none'><input type='button' class='close' value=&times; onclick='togglePopUp()'/>" + message.sender + "<button class='dm'>Direct Message</button></div>";
+        usernameText.innerHTML = "<input type='button' value=" + message.sender + '\xa0\xa0\xa0\xa0\xa0\xa0\xa0' + currentTime + " onclick='togglePopUp()' style='background: none;border: none;padding: 0;text-decoration: underline;cursor: pointer;font-size: 18px;font-weight: 600;color: #cfcfcf;'/><div class='popup' id='pop' style='display: none'><input type='button' class='close' value=&times; onclick='togglePopUp()'/>" + message.sender + "<button class='dm'>Direct Message</button></div>";
         usernameElement.appendChild(usernameText);
         messageElement.appendChild(usernameElement);
     }
